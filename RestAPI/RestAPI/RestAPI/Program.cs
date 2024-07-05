6f801a848ef9c25f4bc40ce3ecec341dce884a87
epzhan <38798908+epzhan@users.noreply.github.com>
2024-07-05 13:57:15 +0800

    add docker file

--- RestAPI/RestAPI/RestAPI/Program.cs
+++ RestAPI/RestAPI/RestAPI/Program.cs
@@ -1,13 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
+ILogger logger = SetupLogger();
+logger.LogInformation("# WAVEFORM API - REST - Start #");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
+logger.LogInformation($"Environment -- {app.Environment}");
+app.MapHealthChecks("/health");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +29,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+ILogger SetupLogger()
+{
+    using var loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder
+        .AddFilter("Microsoft", LogLevel.Warning)
+        .AddFilter("System", LogLevel.Warning)
+        .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+        .AddConsole();
+    });
+
+    ILogger logger = loggerFactory.CreateLogger<Program>();
+    return logger;
+}
