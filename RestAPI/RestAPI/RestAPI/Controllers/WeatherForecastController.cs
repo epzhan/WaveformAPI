34eb638ad3c1dd845f7b981b9ac9f6202bb60001
edwin <edwinpzh@gmail.com>
2025-02-26 22:00:23 +0800

    testing otel

--- RestAPI/RestAPI/RestAPI/Controllers/WeatherForecastController.cs
+++ RestAPI/RestAPI/RestAPI/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace RestAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("WeatherForecast get");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
--- RestAPI/RestAPI/RestAPI/Program.cs
+++ RestAPI/RestAPI/RestAPI/Program.cs
@@ -1,18 +1,85 @@
+using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Exporter;
+//using Serilog;
+//using Serilog.Sinks.Grafana.Loki;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ILogger logger = SetupLogger();
-logger.LogInformation("# WAVEFORM API - REST - Start #");
+//ILogger logger = SetupLogger();
+//logger.LogInformation("# WAVEFORM API - REST - Start #");
 
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService(serviceName: "aaa"))
+var tracingOtlpEndpoint = builder.Configuration["OTLP_ENDPOINT_URL"];
+var resource = ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName);
+
+builder.Services
+    .AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
     .WithMetrics(metrics =>
-    metrics.AddAspNetCoreInstrumentation()
-    .AddRuntimeInstrumentation()
-    .AddPrometheusExporter()
-    );
+
+        metrics
+        .AddAspNetCoreInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddMeter("Microsoft.AspNetCore.Hosting")
+        .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+        .AddMeter("System.Net.Http")
+        .AddMeter("System.Net.NameResolution")
+        .AddConsoleExporter()
+        .AddOtlpExporter(options =>
+        {
+            options.Endpoint = new Uri(tracingOtlpEndpoint);
+            options.Protocol = OtlpExportProtocol.Grpc;
+        })
+    )
+    .WithTracing(tracing =>
+    {
+        tracing
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddConsoleExporter();
+
+        if (tracingOtlpEndpoint != null)
+        {
+            tracing.AddOtlpExporter(otlpOptions =>
+            {
+                otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+                otlpOptions.Protocol = OtlpExportProtocol.Grpc;
+            });
+        }
+        else
+        {
+            tracing.AddConsoleExporter();
+        }
+    });
+
+builder.Logging.ClearProviders();
+builder.Logging.AddOpenTelemetry(logging => {
+    logging
+    .SetResourceBuilder(resource)
+    .AddConsoleExporter()
+    .AddOtlpExporter(options =>
+    {
+        options.Endpoint = new Uri(tracingOtlpEndpoint);
+        options.Protocol = OtlpExportProtocol.Grpc;
+    });
+});
+//// Configure Serilog
+//Log.Logger = new LoggerConfiguration()
+//    .Enrich.FromLogContext()
+//    //.Enrich.WithThreadId()
+//    //.Enrich.WithMachineName()
+//    .WriteTo.Console()
+//    .WriteTo.GrafanaLoki("http://loki:3100", // Loki URL
+//        labels: new[] { new LokiLabel() {
+//            Key = "app",
+//            Value = "my-dotnet-api"
+//        } })
+//    .CreateLogger();
+
+//// Add Serilog to the app
+//builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -23,8 +90,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
-logger.LogInformation($"Environment -- {app.Environment}");
+
+//app.UseSerilogRequestLogging();
+//logger.LogInformation($"Environment -- {app.Environment}");
 app.MapHealthChecks("/health");
+app.MapGet("/", () => {
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("Processing request for / endpoint"); // Example log
+    return "Hello, OTel Collector & Console!";
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -33,10 +107,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseOpenTelemetryPrometheusScrapingEndpoint();
-
+//app.MapPrometheusScrapingEndpoint();
+//app.UseOpenTelemetryPrometheusScrapingEndpoint();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.UseRouting();
@@ -44,17 +117,17 @@ app.UseEndpoints(endpoints => app.MapControllers());
 
 app.Run();
 
-ILogger SetupLogger()
-{
-    using var loggerFactory = LoggerFactory.Create(builder =>
-    {
-        builder
-        .AddFilter("Microsoft", LogLevel.Warning)
-        .AddFilter("System", LogLevel.Warning)
-        .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
-        .AddConsole();
-    });
+//ILogger SetupLogger()
+//{
+//    using var loggerFactory = LoggerFactory.Create(builder =>
+//    {
+//        builder
+//        .AddFilter("Microsoft", LogLevel.Warning)
+//        .AddFilter("System", LogLevel.Warning)
+//        .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+//        .AddConsole();
+//    });
 
-    ILogger logger = loggerFactory.CreateLogger<Program>();
-    return logger;
-}
+//    ILogger logger = loggerFactory.CreateLogger<Program>();
+//    return logger;
+//}
